3a5962d30aedfeee9f3365596eca016c2f441106
Katie Lonsdale <katie952@gmail.com>
2022-12-06 11:21:39 -0700

    add mild challenge

---
+++ code_challenges/mild_challenge.rb
@@ -0,0 +1,11 @@
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even, 
+# and the String "odd" if the Integer is odd.
+
+age = 33
+
+if age.even? == true
+    puts 'even'
+else age.even? != true
+    puts 'odd'
+end
